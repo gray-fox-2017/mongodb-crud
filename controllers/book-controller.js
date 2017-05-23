f1743c8275c3fa1d60b521925c2524e9b00d464a
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-05-23 13:15:44 +0700

    CRUD completed

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,18 +1,7 @@
 'use strict'
 var url = 'mongodb://localhost/library';
 var MongoClient = require('mongodb').MongoClient
-
-var insertBooks = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('books');
-
-  collection.insertMany([
-    req.body
-  ], function(err, result) {
-    res.send("Book inserted into the collection");
-    callback(result);
-  });
-}
+var ObjectId = require('mongodb').ObjectID
 
 var findAll = (req, res) => {
   MongoClient.connect(url, function(err, db) {
@@ -23,7 +12,62 @@ var findAll = (req, res) => {
   });
 }
 
+var newBook = (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('books');
+    collection.insertMany(
+      req.body,
+      function(err, result){
+        res.send(result)
+      }
+    )
+  });
+}
+
+var getOneBook = (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('books');
+    var id = req.params.id
+    var o_id = new ObjectId(id)
+    collection.find({"_id":o_id}).toArray(function(err, docs) {
+      res.send(docs)
+    });
+  });
+}
+
+var deleteBook = (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('books');
+    var id = req.params.id
+    var o_id = new ObjectId(id)
+    collection.deleteOne(
+      {"_id":o_id},
+      function(err, result){
+        res.send("book deleted")
+      }
+    )
+  });
+}
+
+var updateBook = (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('books');
+    var id = req.params.id
+    var o_id = new ObjectId(id)
+    collection.updateOne(
+      {"_id":o_id},
+      { $set: req.body },
+      function(err, result){
+        res.send("book updated")
+      }
+    )
+  });
+}
+
 module.exports = {
-  insertBooks,
-  findAll
+  findAll,
+  newBook,
+  getOneBook,
+  deleteBook,
+  updateBook
 }
